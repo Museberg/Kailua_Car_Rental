7500d42cade69860f235923da7e362496dcc54f3
Museberg <Rasmus@falk-jensen.dk>
2020-04-15 15:45:19 +0200

    Added methods for Date object in DateHelper

--- src/com/KCR/DateHelper.java
+++ src/com/KCR/DateHelper.java
@@ -1,18 +1,33 @@
 package com.KCR;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.*;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class DateHelper {
 
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter localDateformatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final SimpleDateFormat dateformatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    // Returns true if local date can be parsed
+    private static boolean localDateIsValid(String dateStr) {
+        try {
+            LocalDate retDate = LocalDate.parse(dateStr, localDateformatter);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
 
     // Returns true if date can be parsed
-    private static boolean isValid(String dateStr) {
+    private static boolean dateIsValid(String dateStr) {
         try {
-            LocalDate retDate = LocalDate.parse(dateStr, formatter);
-        } catch (DateTimeParseException e) {
+            Date retDate = dateformatter.parse(dateStr);
+        } catch (ParseException e) {
             return false;
         }
         return true;
@@ -23,14 +38,22 @@ public class DateHelper {
         return Period.between(startDate, endDate).getDays();
     }
 
-    public static LocalDate parseDate(String dateStr) {
-        return LocalDate.parse(dateStr, formatter);
+    public static long getDays(Date startDate, Date endDate){
+        return TimeUnit.DAYS.convert(endDate.getTime() - startDate.getTime(), TimeUnit.MILLISECONDS);
+    }
+
+    public static LocalDate parseLocalDate(String dateStr) {
+        return LocalDate.parse(dateStr, localDateformatter);
+    }
+    public static Date parseDate(String dateStr) throws ParseException {
+        return dateformatter.parse(dateStr);
     }
 
     // Returns the date as dd/mm yyyy - e.g. 28/05 2019
     public static String dateToString(LocalDate date) {
-        return date.format(formatter);
+        return date.format(localDateformatter);
     }
+    public static String dateToString(Date date) {return dateformatter.format(date); }
 
     // Asks the user to input a date. Only returns once date in correct format is given
     public static LocalDate getValidLocalDateFromUser(String title) {
@@ -39,13 +62,30 @@ public class DateHelper {
 
         String date = scan.nextLine(); // Getting input from user
 
-        boolean validDate = isValid(date);
+        boolean validDate = localDateIsValid(date);
 
         while (!validDate) {
             System.out.printf("%s does not match with the format yyyy-MM-dd! Please try again.", date);
             System.out.printf("%n%s: ", title);
             date = scan.nextLine();
-            validDate = isValid(date);
+            validDate = localDateIsValid(date);
+        }
+        return parseLocalDate(date);
+    }
+
+    public static Date getValidDateFromUser(String title) throws ParseException {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("%n%s: ", title);
+
+        String date = scan.nextLine(); // Getting input from user
+
+        boolean validDate = dateIsValid(date);
+
+        while (!validDate) {
+            System.out.printf("%s does not match with the format yyyy-MM-dd HH:mm:ss! Please try again.", date);
+            System.out.printf("%n%s: ", title);
+            date = scan.nextLine();
+            validDate = dateIsValid(date);
         }
         return parseDate(date);
     }
